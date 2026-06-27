fe3ba158c7ab89d25ae300dbb73fb8490c631e9c
Satwik K. Udupi <satwik@example.com>
2026-06-27 11:32:23 +0530

    chore: setup netlify deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PanchgangaFloodwatch/',
+  base: './', // Use relative paths so it works on both GitHub Pages and Netlify
   plugins: [react()],
   server: {
     port: 5173,
